51a6b2d55ee58e44ec3f74045382040fc7378c55
CRAIG HUCKERBY <craighckby@gmail.com>
2026-01-08 00:29:37 +1000

    DARLIK_KHAN: EVOLVE

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-// DALEK KHAN ADDENDUM: Ensure environment variable typing is present in a separate global.d.ts for robustness, 
-// but this file remains untouched per Next.js requirements.
